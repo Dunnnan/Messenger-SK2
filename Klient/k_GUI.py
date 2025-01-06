1f89e00e10653f2e5c3cd5d747b7536275b18ab6
owiec142 <147399961+owiec142@users.noreply.github.com>
2025-01-06 14:43:41 +0100

    Add files via upload

--- Klient/k_GUI.py
+++ Klient/k_GUI.py
@@ -8,6 +8,7 @@ import socket
 import threading
 import struct
 import time
+import os
 
 server_address = "127.0.0.2"
 server_port = 1100
@@ -42,12 +43,12 @@ def send_to_server(client_socket,data):
         client_socket.sendall(i.encode('utf-8'))
 
 
-
 # Klasy Worker i WorkerSignals
 class WorkerSignals(QObject):
     result = pyqtSignal(object)
     finished = pyqtSignal()
     error = pyqtSignal(tuple)
+    update_chat = pyqtSignal(list)  # Nowy sygnał do aktualizacji historii chatu
 
 class Worker(QRunnable):
     def __init__(self, fn, *args, **kwargs):
@@ -69,9 +70,231 @@ class Worker(QRunnable):
         finally:
             self.signals.finished.emit()
 
+class main_menu(object):
+
+    def __init__(self, nick=None):
+        self.nick = nick
+        self.signals = WorkerSignals()  # Używamy WorkerSignals zdefiniowanego dla Worker
+        self.signals.update_chat.connect(self.update_chat_history)
+        self.message_to_send = False
+
+    nazwyChaty = []
+    idChaty = []
+
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(533, 396)
+
+        self.comboBox = QtWidgets.QComboBox(parent=Form)
+        self.comboBox.setGeometry(QtCore.QRect(80, 20, 201, 51))
+        self.comboBox.setObjectName("comboBox")
+        self.comboBox.addItem("")
+        self.comboBox.currentIndexChanged.connect(self.update_text_based_on_selection)
+
+        self.textEdit = QtWidgets.QTextEdit(parent=Form)
+        self.textEdit.setGeometry(QtCore.QRect(40, 110, 281, 191))
+        self.textEdit.setObjectName("textEdit")
+        self.textEdit.setReadOnly(True)
+
+        self.lineEdit_2 = QtWidgets.QLineEdit(parent=Form)
+        self.lineEdit_2.setGeometry(QtCore.QRect(70, 320, 201, 21))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+
+        self.pushButton = QtWidgets.QPushButton(parent=Form)
+        self.pushButton.setGeometry(QtCore.QRect(290, 320, 61, 21))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.setText("Send")
+        self.pushButton.clicked.connect(self.send_message)
+
+        # Timer do odświeżania zawartości
+        self.threadpool = QThreadPool()
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.run_receive_history_thread)
+        self.timer.start(2000)  # Co 2 sekundy
+
+        # Plik czatu dla "Option 1"
+        self.chat_file = "chat_option1.txt"
+        self.current_option = ""
+
+        self.mutex = QMutex()
+
+        # Upewnij się, że plik istnieje
+        if not os.path.exists(self.chat_file):
+            with open(self.chat_file, 'w') as file:
+                pass  # Tworzenie pustego pliku
+
+    def run_thread(self, function, *args):
+        worker = Worker(function, *args)
+        worker.signals.result.connect(self.handle_result)
+        worker.signals.error.connect(self.handle_error)
+        worker.signals.update_chat.connect(self.update_chat_history)
+        self.threadpool.start(worker)
+
+
+    def run_receive_history_thread(self):
+        # Uruchom odbieranie historii na wątku
+        self.run_thread(self.sequential_receive)
+
+    def sequential_receive(self):
+        # Synchronizacja wątków za pomocą mutexa
+        print(self.current_option)
+        self.current_option = self.comboBox.currentText()
+        with QMutexLocker(self.mutex):
+            self.receive_history()
+            if self.current_option !="":
+                self.send_to_server(400, self.current_option)
+                self.receive_chat()
+
+            if self.message_to_send:
+                print("HERE!!\n\n")
+                self.send_message_to_server()  # Wyślij wiadomość
+                self.message_to_send = False  # Resetuj flagę
+
+
+
+    def update_text_based_on_selection(self):
+        self.current_option = self.comboBox.currentText()
+        
+    def send_message(self):
+        self.message_to_send = True  # Ustaw flagę na True, aby wysłać wiadomość
+
+    def send_message_to_server(self):
+
+        self.current_option = self.comboBox.currentText()
+
+        message = self.lineEdit_2.text().strip()
+        nick = self.nick
+        chatName=self.current_option
+
+        flag = 0
+        client_socket.send(struct.pack("i", flag))
+        #mess = f'{nick}\n{chatName}\n{message}'
+        #client_socket.send(mess.encode('utf-8'))
+        time.sleep(0.5)
+        client_socket.send(nick.encode('utf-8'))
+        time.sleep(0.5)
+        client_socket.send(chatName.encode('utf-8'))
+        time.sleep(0.5)
+        client_socket.send(message.encode('utf-8'))
+        
+
+
+
+    def send_to_server(self, flag, data):
+        try:
+            client_socket.send(struct.pack("i",flag))
+            print("Data", data)
+            client_socket.send(data.encode('utf-8'))
+            time.sleep(1)
+                
+            return "Message sent."
+        except Exception as e:
+            return f"Error send: {str(e)}"
+
+    def refresh_chat(self):
+        try:
+            with open(self.chat_file, 'r') as file:
+                messages = file.readlines()
+            self.textEdit.setText("".join(messages))
+        except FileNotFoundError:
+            self.textEdit.setText("")
+
+        self.textEdit.moveCursor(QtGui.QTextCursor.MoveOperation.End)
+
+    def receive_history(self):
+        flag = 300
+        client_socket.send(struct.pack("i", flag))
+        client_socket.send(self.nick.encode('utf-8'))
+
+        client_socket.settimeout(1)
+
+        new_nazwyChaty = []
+        new_idChaty = []
+
+        while True:
+            try:
+                mess = client_socket.recv(1024).decode("utf-8", errors='replace').rstrip('\x00')
+                if mess:
+                    new_nazwyChaty.append(mess)
+                else:
+                    print("Server disconnected")
+                    break
+
+                mess = client_socket.recv(1024).decode("utf-8", errors='replace').rstrip('\x00')
+                if mess:
+                    new_idChaty.append(mess)
+                else:
+                    print("Server disconnected")
+                    break
+
+            except socket.timeout:
+                if not new_nazwyChaty:
+                    print("\033[31mUżytkownik nie posiada żadnych chatów !\033[0m")
+                print("Receiving history timed out")
+                break
+            except Exception as e:
+                print(f"An error occurred while receiving history: {e}")
+                break
+
+        if new_nazwyChaty != self.nazwyChaty:
+            self.nazwyChaty = new_nazwyChaty
+            self.idChaty = new_idChaty
+            self.update_combobox()
+    
+    def receive_chat(self):
+        history_received = False
+        chat_history = []  # Lista do przechowywania historii chatu
+
+        # Najpierw odbieramy historię chatu
+        while not history_received:
+            try:
+                # Odbieranie historii (w formacie danych z serwera)
+                mess = client_socket.recv(1024).decode('utf-8', errors='replace')
+                if mess:
+                    print(f"Server: {mess}")
+                    chat_history.append(mess)  # Dodaj wiadomość do historii
+                else:
+                    print("Server disconnected")
+                    break
+            except socket.timeout:
+                print("Receiving message timed out")
+                break
+            except Exception as e:
+                print(f"An error occurred while receiving message: {e}")
+                break
+            else:
+                # Jeśli wiadomość została odebrana pomyślnie
+                history_received = True
+
+        self.signals.update_chat.emit(chat_history)
+
+    def update_chat_history(self, chat_history):
+    # Uaktualnij textEdit w głównym wątku
+        if chat_history:
+            self.textEdit.setText("".join(chat_history))  # Ustaw tekst na wszystkie wiadomości w historii
+        else:
+            self.textEdit.setText("Brak wiadomości w historii.")
+        self.textEdit.moveCursor(QtGui.QTextCursor.MoveOperation.End)
+
+    # Przypisz metodę do sygnału
+    
+    def update_combobox(self):
+        self.comboBox.blockSignals(True)  # Zablokowanie sygnałów podczas aktualizacji
+        self.comboBox.clear()
+        for nazwa in self.nazwyChaty:
+            self.comboBox.addItem(nazwa)
+        self.comboBox.blockSignals(False)  # Odblokowanie sygnałów
+        self.update_text_based_on_selection()
+
+    def handle_result(self, result):
+        print(result)
+
+    def handle_error(self, error):
+        print(f"Error: {error[1]}")
+
 
 class signin(object):
-    
+
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(266, 393)
@@ -179,6 +402,40 @@ class signin(object):
         for i in open_windows:
             print(i)
 
+    def receive_friends(self):
+        client_socket.settimeout(1)
+
+        # Wyczyść listy znajomych
+        nazwyZnajomi = []
+        statusZnajomi = []
+
+        while True:
+            try:
+                mess = client_socket.recv(1024).decode("utf-8",errors='replace')
+                if mess:
+                    print(f"friend: {mess}")
+                    nazwyZnajomi.append(mess)
+                else:
+                    print("Server disconnected")
+                    break
+
+                if mess:
+                    mess = client_socket.recv(1024).decode("utf-8",errors='replace')
+                    print(f"status: {mess}")
+                else:
+                    print("Server disconnected")
+                    break
+
+            except socket.timeout:
+                if not nazwyZnajomi:
+                    print("\033[31mUżytkownik nie posiada żadnych znajomych !\033[0m")
+                print("Server timed out")
+                break
+            except Exception as e:
+                print(f"An error occurred while receiving friends: {e}")
+                break
+
+
     def send_to_server(self, data):
         try:
             flag=200
@@ -214,15 +471,15 @@ class signin(object):
             open_windows=QApplication.topLevelWidgets()
             for i in open_windows:
                 i.close()
-            flag=500
-            client_socket.send(struct.pack("i",flag))
-            client_socket.send(self.nick.encode('utf-8'))
-            rec=client_socket.recv(1024)
-            print(rec.decode())
+
+            # Tworzymy nowe okno i je wyświetlamy
+            self.window = QtWidgets.QWidget()  # Stwórz nowe okno
+            self.ui = main_menu(self.nick)  # Utwórz obiekt klasy signup
+            self.ui.setupUi(self.window)  # Ustaw UI dla tego okna
+            self.window.show()  # Pokaż nowe okno
 
     def handle_error(self, error):
         print(f"Error: {error[1]}")
-
 
 class signup(object):
     def __init__(self, Form):
@@ -382,8 +639,15 @@ except Exception as e:
     exit()
 
 
+def disconnect():
+    flag=-1
+    client_socket.send(struct.pack("i",flag))
+    client_socket.close()
+    exit()
+
 
 app = QtWidgets.QApplication(sys.argv)
+app.aboutToQuit.connect(lambda: disconnect())
 Form = QtWidgets.QWidget()
 ui = signin()
 ui.setupUi(Form)
